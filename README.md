f09847f61ed03444b4bec70d552a5b60804550f6
christina172 <117263200+christina172@users.noreply.github.com>
2023-06-26 13:52:52 +0300

    Make other changes required for deployment and add a README file

---
+++ README.md
@@ -0,0 +1,3 @@
+Express Local Library Tutorial by MDN (The Odin Project, JavaScript Path, Node.js Course, Express & Mongoose)
+
+This is an MDN tutorial to create a library website in Node/Express (using MongoDB and Pug view engine and deploying on Railway).
--- app.js
+++ app.js
@@ -27,7 +27,8 @@ app.use(limiter);
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = process.env.CONNECTION_STRING;
+const dev_db_url = process.env.CONNECTION_STRING;
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch((err) => console.log(err));
 async function main() {
